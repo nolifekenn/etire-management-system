c4928545e1d8256237c5a058e3077a73e3cde788
GLIPONEO8 <kurtrusselgliponeo@gmail.com>
2025-12-05 14:23:09 +0800

    Change design in purchasing.tsx

--- etire-management-system/src/app/purchasing/page.tsx
+++ etire-management-system/src/app/purchasing/page.tsx
@@ -2277,7 +2277,7 @@ export default function EnhancedPurchasingPage() {
         <div 
           className="absolute inset-0 rounded-b-[40px] bg-cover bg-center"
           style={{ 
-            backgroundImage: "url('/images/image7.png')",
+            backgroundImage: "url('/images/image2.jpg')",
             backgroundSize: "cover",
             backgroundPosition: "center 30%"
           }}
